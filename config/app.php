3868627a93b954f3dca3f8ec35f9ede90a134522
Vidwa-Yasahara <yasaharav@gmail.com>
2026-02-02 11:36:30 +0530

    Add fallback APP_KEY for production stability

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:c4twST5f0bwyEgRN+hL320sbfDEffRNCEpiLXCpZdvc='),
 
     'previous_keys' => [
         ...array_filter(
